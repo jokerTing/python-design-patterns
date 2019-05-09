e93c2d0846f6a7283e78a492ca0e03fc5fdf53b4
shadowDing <dhc_1991@qq.com>
2019-05-09 12:08:57 +0800

    16.2
    更新策略模式

---
+++ strategy/func_strategy.py
@@ -0,0 +1,18 @@
+def executor(arg1, arg2, func=None):
+    if func is None:
+        return "Strategy not implemented..."
+    return func(arg1, arg2)
+
+def strategy_addition(arg1, arg2):
+    return arg1 + arg2
+
+def strategy_subtraction(arg1, arg2):
+    return arg1 - arg2
+
+def main():
+    print(executor(4, 6))
+    print(executor(4, 6, strategy_addition))
+    print(executor(4, 6, strategy_subtraction))
+
+if __name__ == "__main__":
+    main()
