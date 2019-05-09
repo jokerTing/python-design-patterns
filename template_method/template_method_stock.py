19fe157b415e6a763f94548f29ffd976c998acf9
shadowDing <dhc_1991@qq.com>
2019-05-09 19:42:14 +0800

    17.3
    调整格式

--- template_method/template_method_stock.py
+++ template_method/template_method_stock.py
@@ -1,76 +1,82 @@
 import abc
+
+
 class ThirdPartyInteractionTemplate(metaclass=abc.ABCMeta):
     def sync_stock_items(self):
         self._sync_stock_items_step_1()
         self._sync_stock_items_step_2()
         self._sync_stock_items_step_3()
         self._sync_stock_items_step_4()
-    
+
     def send_transaction(self, transaction):
         self._send_transaction(transaction)
-    
+
     @abc.abstractmethod
     def _sync_stock_items_step_1(self): pass
-    
+
     @abc.abstractmethod
     def _sync_stock_items_step_2(self): pass
-    
+
     @abc.abstractmethod
     def _sync_stock_items_step_3(self): pass
-    
+
     @abc.abstractmethod
     def _sync_stock_items_step_4(self): pass
-    
+
     @abc.abstractmethod
     def _send_transaction(self, transaction): pass
-    
+
+
 class System1(ThirdPartyInteractionTemplate):
     def _sync_stock_items_step_1(self):
         print("running stock sync between local and remote system1")
-    
+
     def _sync_stock_items_step_2(self):
         print("retrieving remote stock items from system1")
-    
+
     def _sync_stock_items_step_3(self):
         print("updating local items")
-    
+
     def _sync_stock_items_step_4(self):
         print("sending updates to third party system1")
-    
+
     def _send_transaction(self, transaction):
         print("send transaction to system1: {0!r}".format(transaction))
+
 
 class System2(ThirdPartyInteractionTemplate):
     def _sync_stock_items_step_1(self):
         print("running stock sync between local and remote system2")
-    
+
     def _sync_stock_items_step_2(self):
         print("retrieving remote stock items from system2")
-    
+
     def _sync_stock_items_step_3(self):
         print("updating local items")
-    
+
     def _sync_stock_items_step_4(self):
         print("sending updates to third party system2")
-    
+
     def _send_transaction(self, transaction):
         print("send transaction to system2: {0!r}".format(transaction))
+
 
 class System3(ThirdPartyInteractionTemplate):
     def _sync_stock_items_step_1(self):
         print("running stock sync between local and remote system3")
-    
+
     def _sync_stock_items_step_2(self):
         print("retrieving remote stock items from system3")
-    
+
     def _sync_stock_items_step_3(self):
         print("updating local items")
-    
+
     def _sync_stock_items_step_4(self):
         print("sending updates to third party system3")
-    
+
     def _send_transaction(self, transaction):
         print("send transaction to system3: {0!r}".format(transaction))
+
 
 def main():
     transaction = {
@@ -86,6 +92,7 @@ def main():
         system = C()
         system.sync_stock_items()
         system.send_transaction(transaction)
-    
+
+
 if __name__ == "__main__":
     main()
